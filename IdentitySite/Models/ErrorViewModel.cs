23aa2a017182004c28564b5eb7ff3797694a557c
JohnnyBee86 <BartholomewJohn86@gmail.com>
2023-10-02 16:33:13 -0700

    Add project files.

---
+++ IdentitySite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentitySite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
